d9f050b3774d49b8931e259b00698a7eca4693a9
ankhilko <fieldstrret.27@gmail.com>
2022-12-12 12:48:09 -0500

    added posts

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from django.utils.timezone import now
 
 # Create your models here.
@@ -65,6 +66,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog_post', kwargs={'post_slug': self.slug})
+
 
     # можем использовать вместо date_created
     @property
